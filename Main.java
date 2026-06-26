f5872cbf1b1f9e90148256252172a2c69915199e
emaldev <261611436+emaldev@users.noreply.github.com>
2026-06-26 18:21:42 +0430

    java try-catch in for loop Student age name and height

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         
-       for(int i = 0; i <= 3; i++){
+       for(int i = 1; i <= 3; i++){
+        try{
         System.out.println("Processing student number successfully: ");
         System.out.println("Enter name of student " + (i + 1));
         String name = input.nextLine();
@@ -15,12 +16,24 @@ public class Main {
         System.out.println("Studnt " + i + "has been registred");
         
         System.out.println("===========================<<<>>>================================");        
-        System.out.println(  "|| "+"Name : " + name + ", Age: " + age + ", Height: "+ height +"||"); 
+        System.out.println(  "|| "+" \t Name : " + name + ", \tAge: " + age + ", \tHeight: "+ height +"||"); 
         System.out.println("===========================<<<>>>================================");  
         
+        
+        }catch(Exception e ){
+            System.out.println("Invalid : please enter number only input!");
+            input.nextLine();
+            i--;
+        }
+        
+        finally{
+            System.out.println("Finish program.");
+        }
        }
        System.out.println("All students have been procssed");
-       
+     
+       input.close();
     }
+    
     
 }

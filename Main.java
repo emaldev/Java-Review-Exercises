056d8421f33d757da017d5ca02a804d87690c1cf
emaldev <261611436+emaldev@users.noreply.github.com>
2026-06-26 18:08:19 +0430

    Step3_Loops

--- Main.java
+++ Main.java
@@ -2,16 +2,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter yout Name :");
-        String name = input.nextLine();
         
-        System.out.println("Enter your Age: ");
+       for(int i = 0; i <= 3; i++){
+        System.out.println("Processing student number successfully: ");
+        System.out.println("Enter name of student " + (i + 1));
+        String name = input.nextLine();
+         System.out.println("Enter age of student : " +  (i + 1));
         int age = input.nextInt();
-        System.out.println("Enter your hight: ");
-        double Height = input.nextInt();
-
-        System.out.println("\t Hi Welcom .");
-        System.out.println( "Name : " + name + " this is your age : " + age + "and this is yout Height: " + Height);
+        System.out.println("Enter Height of student" + (i + 1));
+        double height = input.nextDouble();
+        input.nextLine();
+        System.out.println("Studnt " + i + "has been registred");
+        
+        System.out.println("===========================<<<>>>================================");        
+        System.out.println(  "|| "+"Name : " + name + ", Age: " + age + ", Height: "+ height +"||"); 
+        System.out.println("===========================<<<>>>================================");  
+        
+       }
+       System.out.println("All students have been procssed");
+       
     }
     
 }
